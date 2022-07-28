dbd61f1d56d8712d708b654ca50bb3e83b8cab31
tjeaneric <ericjohn415@gmail.com>
2022-07-28 15:41:58 +0200

    fetched data from graphql api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,38 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import { CardItem } from "../components/cards/CardItem";
 
-const Home: NextPage = () => {
+export const getStaticProps = async () => {
+  const client = new ApolloClient({
+    uri: "https://flashcards-graphql-api.herokuapp.com/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  let skip: number = 0;
+
+  const { data } = await client.query({
+    query: gql`
+      {
+        cards(take: 1, skip: ${skip}) {
+          cards {
+            id
+            name
+            description
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      cards: data.cards.cards,
+    },
+  };
+};
+
+const Home: NextPage = ({ cards }: any) => {
   return (
     <div>
       <Head>
@@ -10,9 +40,8 @@ const Home: NextPage = () => {
         <meta name="description" content="Flashcards Nextjs app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
-        <CardItem />
-      </main>
+
+      <CardItem cards={cards} />
     </div>
   );
 };
