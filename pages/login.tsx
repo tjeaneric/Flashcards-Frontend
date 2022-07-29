5898c96c88a3f52e1a1d5d669f681ecb6e90b2f3
tjeaneric <ericjohn415@gmail.com>
2022-07-29 22:15:40 +0200

    Fixed access on login page only if user is not logged in

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 import type { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 import AuthContext from "../store/auth-context";
 
 const LoginMutation = gql`
@@ -22,6 +22,7 @@ const LoginMutation = gql`
 const Login: NextPage = () => {
   const router = useRouter();
   const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
 
   const {
     register,
@@ -55,54 +56,63 @@ const Login: NextPage = () => {
   if (data) router.push("./");
 
   return (
-    <form action="#" method="POST" onSubmit={handleSubmit(onSubmit)}>
-      <div className="login ">
-        <Toaster />
-        <div className="pt-10">
-          <p className="text-3xl font-semibold text-brightOrange">Login</p>
-          <input
-            className="login-input mt-16"
-            type="email"
-            {...register("email", { required: true })}
-            placeholder="Email "
-          />
+    <Fragment>
+      <>
+        {!isLoggedIn && (
+          <form action="#" method="POST" onSubmit={handleSubmit(onSubmit)}>
+            <div className="login ">
+              <Toaster />
+              <div className="pt-10">
+                <p className="text-3xl font-semibold text-brightOrange">
+                  Login
+                </p>
+                <input
+                  className="login-input mt-16"
+                  type="email"
+                  {...register("email", { required: true })}
+                  placeholder="Email "
+                />
 
-          <input
-            className="login-input"
-            type="password"
-            placeholder="Password"
-            {...register("password", { required: true })}
-          />
-          <button
-            disabled={loading}
-            type="submit"
-            className="btn login-btn font-semibold bg-brightRed hover:bg-brightOrange rounded-full"
-          >
-            {loading ? (
-              <span className="flex items-center justify-center">
-                <svg
-                  className="w-6 h-6 animate-spin mr-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+                <input
+                  className="login-input"
+                  type="password"
+                  placeholder="Password"
+                  {...register("password", { required: true })}
+                />
+                <button
+                  disabled={loading}
+                  type="submit"
+                  className="btn login-btn font-semibold bg-brightRed hover:bg-brightOrange rounded-full"
                 >
-                  <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
-                </svg>
-                Logging in...
-              </span>
-            ) : (
-              <span>Login</span>
-            )}
-          </button>
-          <div className="text-xl">
-            <p>Don&apos;t have an account?</p>
-            <div className="underline font-bold text-brightRed">
-              <Link href="./register"> Register</Link>
+                  {loading ? (
+                    <span className="flex items-center justify-center">
+                      <svg
+                        className="w-6 h-6 animate-spin mr-1"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
+                      </svg>
+                      Logging in...
+                    </span>
+                  ) : (
+                    <span>Login</span>
+                  )}
+                </button>
+                <div className="text-xl">
+                  <p>Don&apos;t have an account?</p>
+                  <div className="underline font-bold text-brightRed">
+                    <Link href="./register"> Register</Link>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </form>
+          </form>
+        )}
+        {isLoggedIn && router.replace("./")}
+      </>
+    </Fragment>
   );
 };
 
