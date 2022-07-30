523e9c0232a65728b5e49bc0c08cad7056b1b29e
tjeaneric <ericjohn415@gmail.com>
2022-07-30 23:24:25 +0200

    defined handler functions for prev and next buttons

--- components/cards/CardItem.tsx
+++ components/cards/CardItem.tsx
@@ -1,17 +1,39 @@
-export const CardItem = ({ cards }: any) => {
+export const CardItem = ({ cards, onSkip }: any) => {
+  console.log(cards.count);
+  const nextCardHandler = () => {
+    if (onSkip < cards.count) {
+      onSkip++;
+    } else onSkip = 0;
+    console.log(onSkip);
+  };
+
+  const prevCardHandler = () => {
+    if (onSkip !== 0) {
+      onSkip--;
+    } else onSkip = cards.count;
+    console.log(onSkip);
+  };
   return (
     <div className=" flex flex-col items-center justify-center">
       <div className="card mt-20">
-        <div className="card__side card__side--front">{cards[0].name}</div>
+        <div className="card__side card__side--front">
+          {cards.cards[0].name}
+        </div>
         <div className="card__side card__side--back">
-          {cards[0].description}
+          {cards.cards[0].description}
         </div>
       </div>
       <div className="flex items-center justify-between space-x-24 mt-6">
-        <div className="rounded-md text-xl cursor-pointer bg-greyLight p-3 px-10 py-2 text-black hover:bg-brightRed hover:text-white ">
+        <div
+          onClick={prevCardHandler}
+          className="rounded-md text-xl cursor-pointer bg-greyLight p-3 px-10 py-2 text-black hover:bg-brightRed hover:text-white "
+        >
           prev
         </div>
-        <div className="rounded-md text-xl bg-greyLight cursor-pointer p-3 px-10 py-2 text-black hover:bg-brightRed hover:text-white">
+        <div
+          onClick={nextCardHandler}
+          className="rounded-md text-xl bg-greyLight cursor-pointer p-3 px-10 py-2 text-black hover:bg-brightRed hover:text-white"
+        >
           next
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,24 @@ import Head from "next/head";
 import { useQuery, gql } from "@apollo/client";
 import { CardItem } from "../components/cards/CardItem";
 
-const skip: number = 1;
+let skip: number = 1;
 
 const cardData = gql`
-     query {
-        cards(take: 1, skip: ${skip}) {
-          cards {
-            id
-            name
-            description
-          }
-        }
-      }
-    `;
+query {
+   cards(take: 1, skip: ${skip}) {
+     count
+     cards {
+       id
+       name
+       description
+     }
+   }
+ }
+`;
 
 const Home: NextPage = () => {
   const { data, loading, error } = useQuery(cardData);
+  if (!loading) console.log(data.cards);
 
   if (error) return <p>Oh no... {error.message}</p>;
 
@@ -30,7 +32,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {loading ? <p>Loading...</p> : <CardItem cards={data.cards.cards} />}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <CardItem cards={data.cards} onSkip={skip} />
+      )}
     </div>
   );
 };
